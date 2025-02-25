0b5d67f34562a56f281c8163621ead686baadeb9
张斌 <bin.zhang@appshahe.com>
2025-02-25 15:08:34 +0800

    feat：节点常量

---
+++ Assets/Runtime/Const/FSMConst.cs
@@ -0,0 +1,30 @@
+﻿namespace MHFSM
+{
+    public class FSMConst
+    {
+        /// <summary>
+        /// 任意状态，用于状态机中表示任意一个状态
+        /// </summary>
+        public const string AnyState = "Any State";
+        
+        /// <summary>
+        /// 入口状态，用于状态机中表示初始状态
+        /// </summary>
+        public const string EntryState = "Entry";
+        
+        /// <summary>
+        /// 上一层
+        /// </summary>
+        public const string Up = "Up";
+        
+        /// <summary>
+        /// 状态节点宽度
+        /// </summary>
+        public const int StateNodeWith = 200;
+        
+        /// <summary>
+        /// 状态节点高度
+        /// </summary>
+        public const int StateNodeHeight = 400;
+    }
+}
