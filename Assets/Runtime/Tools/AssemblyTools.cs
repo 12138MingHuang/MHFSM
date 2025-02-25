d7f93fa44b217c57e6aa8ffaabf3eaafd7baed80
张斌 <bin.zhang@appshahe.com>
2025-02-25 16:18:37 +0800

    feat：两工具类

---
+++ Assets/Runtime/Tools/AssemblyTools.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+
+namespace MHFSM
+{
+    /// <summary>
+    /// 程序集工具
+    /// </summary>
+    public class AssemblyTool
+    {
+        private static Dictionary<string, Type> _typeCaches = new Dictionary<string, Type>();
+
+        /// <summary>
+        /// 根据名称来获取类型信息，如果找不到则返回null。
+        /// </summary>
+        /// <param name="classFullName"> 类型全名(含命名空间)</param>
+        /// <returns> 类型信息 </returns>
+        internal static Type GetType(string classFullName)
+        {
+            if(string.IsNullOrEmpty(classFullName)) return null;
+            
+            if(_typeCaches.ContainsKey(classFullName) && _typeCaches[classFullName] != null) return _typeCaches[classFullName];
+            
+            _typeCaches.Remove(classFullName);
+            
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                Assembly assembly = assemblies[i];
+                Type type = assembly.GetType(classFullName);
+                if(type != null)
+                {
+                    _typeCaches.Add(classFullName, type);
+                    break;
+                }
+            }
+            
+            return _typeCaches.GetValueOrDefault(classFullName);
+
+        }
+
+#if UNITY_EDITOR
+
+        /// <summary>
+        /// 获取所有状态脚本(仅仅编辑器有效)
+        /// </summary>
+        /// <returns></returns>
+        public static List<MonoScript> GetAllStatesType()
+        {
+            List<MonoScript> states = new List<MonoScript>();
+            
+            string[] scripts = AssetDatabase.FindAssets("t:Script");
+
+            foreach (string script in scripts)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(script);
+                MonoScript monoScript = AssetDatabase.LoadAssetAtPath<MonoScript>(path);
+                
+                if(monoScript == null) continue;
+
+                Type type = monoScript.GetClass();
+                
+                if(type == null) continue;
+                
+                if(type.IsSubclassOf(typeof(FSMState)))
+                   states.Add(monoScript);
+            }
+            
+            return states;
+        }
+
+        /// <summary>
+        /// 根据状态脚本全名称获取脚本guid(仅编辑器有效)
+        /// </summary>
+        /// <param name="fullName"> 状态脚本全名称 </param>
+        /// <returns> 脚本guid </returns>
+        public static string GetGUIDByStateClassFullNmae(string fullName)
+        {
+            List<MonoScript> scripts = GetAllStatesType();
+
+            foreach (MonoScript script in scripts)
+            {
+                if (script.GetClass() != null && script.GetClass().FullName!.Equals(fullName))
+                {
+                    return AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(script));
+                }
+            }
+            
+            return string.Empty;
+        }
+#endif
+    }
+}
---
+++ Assets/Runtime/Tools/EditorApplicationTool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace MHFSM
+{
+    /// <summary>
+    /// EditorApplicationTool工具类，用于编辑器相关功能封装。
+    /// </summary>
+    internal class EditorApplicationTool
+    {
+        /// <summary>
+        /// 默认的编辑器播放状态。
+        /// </summary>
+        private static bool _isPlaying = true;
+
+        /// <summary>
+        /// 判断当前编辑器是否处于运行状态(UnityEngine.Application.isPlaying在停止运行编辑器时触发OnDestroy时仍返回true)
+        /// </summary>
+        public static bool isPlaying
+        {
+            get { return _isPlaying; }
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init()
+        {
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+#endif
+        }
+        
+#if UNITY_EDITOR
+        private static void EditorApplication_playModeStateChanged(PlayModeStateChange obj)
+        {
+            switch (obj)
+            {
+                case PlayModeStateChange.EnteredEditMode:
+                    _isPlaying = false;
+                    break;
+                case PlayModeStateChange.EnteredPlayMode:
+                    _isPlaying = true;
+                    break;
+                case PlayModeStateChange.ExitingEditMode:
+                    _isPlaying = true;
+                    break;
+                case PlayModeStateChange.ExitingPlayMode:
+                    _isPlaying = false;
+                    break;
+            }
+        }
+#endif
+    }
+}
